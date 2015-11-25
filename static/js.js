217d67bbaeaf0d3e3bcc479fa2ddf011eea1a828
jqjjian-pc <jqjjian@126.com>
2015-11-25 15:31:43 +0800

    666
    666

--- static/js.js
+++ static/js.js
@@ -3,7 +3,7 @@ return {
 		/* begin 基础模块 */
 		'{static}/web-tvbh/static/js/base.mod.js': [
 			'{static}/public/js/jquery.min.js',
-			// '{static}/static/public/js/jquery.x.min.js',
+			'{static}/static/public/js/jquery.x.min.js',
 			'{static}/web-tvbh/static/js/base.min.js',
 			'{static}/web-tvbh/static/js/baseAjax.min.js'
 		]
